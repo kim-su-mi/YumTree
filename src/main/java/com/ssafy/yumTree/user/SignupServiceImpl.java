d912c1106c539e08909283f5d401cbe262a96b91
kimsumi <ksmm9607@naver.com>
2025-05-20 17:47:17 +0900

    fix : sumi_signup

--- src/main/java/com/ssafy/yumTree/user/SignupServiceImpl.java
+++ src/main/java/com/ssafy/yumTree/user/SignupServiceImpl.java
@@ -19,10 +19,12 @@ public class SignupServiceImpl implements SignupService {
 
         String userId = userDto.getUserId();
 
+        System.out.println(111111111);
         int isExist = signupDao.existsByUserId(userId);
+        System.out.println(222222222);
 
         if (isExist > 0) {
-
+        	System.out.println("중복 ID");
             return;
         }
 
@@ -34,12 +36,21 @@ public class SignupServiceImpl implements SignupService {
 
 //        user.setUserRole("ROLE_ADMIN");
         userDto.setUserPw(bCryptPasswordEncoder.encode(userDto.getUserPw()));
+//        userDto.setUserRole("ROLE_ADMIN");
         userDto.setUserRole("ROLE_USER");
+        
+        userDto.setUserTargetCalories(calculateCal(userDto.getUserHeight(),userDto.getUserWeight(),userDto.getUserActivityLevel()));
 
-        System.out.println("1 : "+userDto);
+        System.out.println(userDto);
         
         
         signupDao.insertUser(userDto);
+    }
+    
+    // 권장 칼로리 계산
+    public int calculateCal(int height,int weight,int activity) {
+    	
+    	return (int)Math.round(((height-100)*0.9)*activity);
     }
 
 }
